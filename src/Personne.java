3b4ed3236220dc39a17a1c4258c3b5d59436e5b6
providjek <providence.djekoundakom@2023.ucac-icam.com>
2024-04-01 22:10:59 +0100

    First commit

---
+++ src/Personne.java
@@ -0,0 +1,11 @@
+
+public class Personne {
+	
+	public String nom = "";
+	public String prenom = "";
+
+	public void direBonjour() {
+	 System.out.println("Bonjour, je m'appelle " + this.nom + " " +
+	this.prenom);
+	}
+}
