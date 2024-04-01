fb2accaccedf24142989c5a3e429d11a83203aea
providjek <providence.djekoundakom@2023.ucac-icam.com>
2024-04-01 13:17:18 +0100

    first commit

---
+++ Workshop1/src/Workshop.java
@@ -0,0 +1,22 @@
+
+public class Workshop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello World");
+		 
+		 Personne personne = new Personne();
+		 personne.nom = "Gilbert";
+		 
+		 System.out.println(personne.nom);
+		 
+
+		 Personne unePersonne = new Personne();
+		 unePersonne.nom = "Diet";
+		 unePersonne.prenom = "Jean-Aymeric";
+		 unePersonne.direBonjour(); 
+
+
+	}
+
+}
